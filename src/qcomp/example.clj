7f2c9dfd95367167ba367e0c1156e416d0d0e5cf
Ignacio Blasco <elnopintan@gmail.com>
2011-11-21 22:47:05 +0100

    bug

--- src/qcomp/example.clj
+++ src/qcomp/example.clj
@@ -2,14 +2,17 @@
   (use qcomp.qbits))
 
 
-(defn ufn [f] (fn [x y] [x (bit-xor x (f y))]))
+(defn ufn [f] (fn [x y] [x (bit-xor y (f x))]))
 
 (def eq identity)
 (def swap #(- 1 %))
 (def con0 (fn [_] 0))
 (def con1 (fn [_] 1))
 
-(defn deutsch [f] (compgate (qgate h h) (qgate (ufn f)) (qgate h id)))
+(defn deutsch [f] (compgate (qgate h h)
+                            (qgate (ufn f))
+                            (qgate h id)
+                            ))
 
 (defn run-deutsch [f] (run 0 1 (deutsch f)))
 
